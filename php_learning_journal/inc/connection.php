1c6661f73ffc0f814d740a456b6cb6dd59b85125
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2020-12-31 15:12:20 -0500

    adds try catch to connection.php creates functions.php in inc folder

--- php_learning_journal/inc/connection.php
+++ php_learning_journal/inc/connection.php
@@ -1,4 +1,11 @@
 <?php
+try {
+    $db = new PDO("sqlite:".__DIR__."/journal.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit();
+}
 
-$db = new PDO("sqlite:".__DIR__."/journal.db");
-var_dump($db);
+
+
