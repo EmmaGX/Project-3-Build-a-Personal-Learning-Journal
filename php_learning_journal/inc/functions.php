68062164485f6b78eee08e7e3dd65da278168339
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2020-12-31 17:11:33 -0500

    creates functions.php, adds function to get title and date for the index.php page to disply the items using the DB. Moves DB inside the connection folder

--- php_learning_journal/inc/functions.php
+++ php_learning_journal/inc/functions.php
@@ -1,1 +1,12 @@
 <?php
+function get_journal_entries() {
+    include 'connection.php';
+
+    try {
+        return $db->query('SELECT title,date FROM entries');
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage() . "</br>";
+        return false;
+    }
+}
+
--- php_learning_journal/index.php
+++ php_learning_journal/index.php
@@ -1,25 +1,20 @@
 <?php
 include "inc/header.php";
+include "inc/functions.php";
 ?>
 
         <section>
             <div class="container">
                 <div class="entry-list">
                     <article>
-                        <h2><a href="detail.php">The best day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_2.html">The absolute worst day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_3.html">That time at the mall</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_4.html">Dude, where’s my car?</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
+                        <?php
+                        foreach (get_journal_entries() as $entry) {
+
+                            echo "<h2>" . $entry['title'] . "</h2>";
+                            $entry_date = date("F,d,Y", strtotime($entry['date']));
+                            echo "<p>" . $entry_date . "</p><br />";
+                        }
+                        ?>
                     </article>
                 </div>
             </div>
