8634e067e111afa4f2c271d819ebfe49ff93c498
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2021-01-01 11:17:08 -0500

    includes <a> to direct the query results to the detail page

--- php_learning_journal/index.php
+++ php_learning_journal/index.php
@@ -10,12 +10,29 @@ include "inc/functions.php";
                         <?php
                         foreach (get_journal_entries() as $entry) {
 
-                            echo "<h2>" . $entry['title'] . "</h2>";
+                            echo "<h2><a href=\"detail.php\">" . $entry['title'] . "</a></h2>";
                             $entry_date = date("F,d,Y", strtotime($entry['date']));
                             echo "<p>" . $entry_date . "</p><br />";
+                            echo "<br />";
+
                         }
                         ?>
+
+<!--                        <h2><a href="detail.php">The best day I’ve ever had</a></h2><time datetime="2016-01-31">January 31, 2016</time>-->
                     </article>
+<!--                    <article>-->
+<!--                        <h2><a href="detail_2.html">The absolute worst day I’ve ever had</a></h2>-->
+<!--                        <time datetime="2016-01-31">January 31, 2016</time>-->
+<!--                    </article>-->
+<!--                    <article>-->
+<!--                        <h2><a href="detail_3.html">That time at the mall</a></h2>-->
+<!--                        <time datetime="2016-01-31">January 31, 2016</time>-->
+<!--                    </article>-->
+<!--                    <article>-->
+<!--                        <h2><a href="detail_4.html">Dude, where’s my car?</a></h2>-->
+<!--                        <time datetime="2016-01-31">January 31, 2016</time>-->
+<!--                    </article>-->
+
                 </div>
             </div>
         </section>
